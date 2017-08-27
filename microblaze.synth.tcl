efad5e36c99b82ddb417b2afd42c529a33771320
grecian_urn <ljzhu_fdu@163.com>
2017-08-27 19:19:31 +0800

    SPI tested on the board

--- microblaze.synth.tcl
+++ microblaze.synth.tcl
@@ -1,5 +1,5 @@
 proc pnsynth {} {
-  cd F:/Programs/Verilog/FPGA_Group/test_br0101/microblaze
+  cd H:/ljzhu/test_br0101/microblaze
   if { [ catch { xload xmp microblaze.xmp } result ] } {
     exit 10
   }
